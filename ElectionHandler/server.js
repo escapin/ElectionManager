15d5fd26f5cf78515b7ebd59b55643f3ded8b152
William Standard <s4wistan@uni-trier.de>
2016-03-10 15:36:41 +0100

    will ask the user for an adminiistrator password if there is no existing one

--- ElectionHandler/server.js
+++ ElectionHandler/server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var bcrypt = require("bcryptjs");
 
+var read = require('read');
 var fs = require('fs');
 var http = require('http');
 //var https = require('https');
@@ -26,6 +27,30 @@ app.use(bodyParser.urlencoded({
 //var httpsserver = https.createServer(credentials, app);
 //var basicAuth = require('basic-auth-connect');
 //app.use('/election/*', basicAuth('admin', '888')); // authentication for the admin panel only
+
+/**process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var util = require('util');
+var adminpw = "";
+var verify = 0;
+process.stdin.on('data', function (text) {
+	if (text == ""){
+		console.log('password cannot be empty, try again:');
+	}
+	else if (verify == 0){
+		verify = 1;
+		adminpw = text;
+		console.log('reenter password:');
+	}
+	else if (text == adminpw){
+		console.log('successfully entered password');
+	}
+	else {
+		verify = 0;
+		adminpw = "";
+		console.log('passwords do not match, try again:');
+	}
+});**/
 
 app.post('/election', function(req, res) {
 	var task = req.body.task;
@@ -80,10 +105,10 @@ app.post('/election', function(req, res) {
 		session = spawn('python', ['../'+direc+'ElectionSetup/NewSession.py', startingTime, endingTime, etitle, edesc]);
 		
 		session.stdout.on('data', function (data) {
-			if(data.indexOf("OTP")>-1){
+			if(String(data).indexOf("OTP")>-1){
 				console.log('stdout: ' + data);
 			}
-			else if(data.indexOf("TLS")>-1){
+			else if(String(data).indexOf("TLS")>-1){
 				console.log('mix stdout: ' + data);
 			}
 		});
@@ -164,8 +189,51 @@ app.post('/election', function(req, res) {
 	}
 });
 
-var server = app.listen(port, function() {
-    console.log('Serving %s on %s, port %d', path, server.address().address, server.address().port);
-});
 
+// Test if the file with stored passwords exists and is a valid json file
+try{
+	var passFile = JSON.parse(fs.readFileSync("_data_/pass.json"));
+	start();
+}
+catch(e){	//if not, remove (if existing but broken json file) and ask for an admin password
+	try{
+		fs.unlinkSync('_data_/pass.json');
+	}
+	catch(e){
+	}
+	read({ prompt: 'Enter an administrator password to remove any election:', silent: true }, function(er, password) {
+	    console.log('You entered: %s', password);
+	    verify(password);
+	 })
+}
 
+// call when user couldn't confirm the password
+function askPw(){
+	read({ prompt: 'passwords do not match, try again:', silent: true }, function(er, password) {
+	    verify(password);
+	  })
+}
+ 
+// confirm the entered password
+function verify(passwd){
+    read({ prompt: 'confirm password: ', silent: true }, function(er, password) {
+        if (password !== passwd){
+        	askPw();
+        }
+        else {
+      	  console.log('successfully entered password');
+      	  var salt = bcrypt.genSaltSync();
+      	  var adminpw = bcrypt.hashSync(password, salt);
+      	  var obj = {adminpassword: adminpw}
+      	  fs.writeFileSync('_data_/pass.json', JSON.stringify(obj, null, 4), {spaces:4});
+      	  start();
+        }
+      })
+}
+
+// start the services
+function start(){
+	var server = app.listen(port, function() {
+	    console.log('Serving %s on %s, port %d', path, server.address().address, server.address().port);
+	});
+}
