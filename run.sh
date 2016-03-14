99885c8892015c6de65a3c635476224c589ba9d1
escapin <me@escapin.net>
2016-03-14 11:41:19 +0100

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+# getting the file location of this file i.e., run.sh
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
   DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
   SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" 
+# if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-make restart >/dev/null
+
+
 echo Starting nginx services managing the sElect servers...
-cd templates/ ; ./nginx_start.sh 2>/dev/null
-cd $DIR
+/usr/sbin/nginx -c $DIR/nginx_config/handler/nginx_select.conf
+
+
 echo Starting the ElectionHandler...
 cd ElectionHandler/ ; ./run.sh
