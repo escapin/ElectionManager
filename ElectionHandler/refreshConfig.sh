2bd568570bf984f2107d3392aacf8913f45acfc6
William Standard <s4wistan@uni-trier.de>
2016-03-16 07:41:09 +0100

    updated file paths

--- ElectionHandler/refreshConfig.sh
+++ ElectionHandler/refreshConfig.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 node ../sElect/tools/config2js.js config.json > webapp/js/config.js configRaw
-node ../sElect/tools/config2js.js ../_handlerConfigFiles_/ElectionConfigFile.json > webapp/js/ElectionConfigFile.js electionConfigRaw
+node ../sElect/tools/config2js.js ../_handlerConfigFiles_/handlerConfigFile.json > webapp/js/ElectionConfigFile.js electionConfigRaw
--- ElectionHandler/run.sh
+++ ElectionHandler/run.sh
@@ -1,3 +1,3 @@
 node ../sElect/tools/config2js.js config.json > webapp/js/config.js configRaw
-node ../sElect/tools/config2js.js ../ElectionConfigFile.json > webapp/js/ElectionConfigFile.js electionConfigRaw
+node ../sElect/tools/config2js.js ../_handlerConfigFiles_/handlerConfigFile.json > webapp/js/ElectionConfigFile.js electionConfigRaw
 node server.js
