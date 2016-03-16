54d38ed24d4b2f1bb39d75a8f807a6eb65641bcd
William Standard <s4wistan@uni-trier.de>
2016-03-16 13:33:33 +0100

    truncate file before closing, avoids overlapping of old lines

--- configNginx.py
+++ configNginx.py
@@ -1,36 +1,44 @@
 import os
 import sys
+import json
+import collections
 
 
 def writeDirec(src, dest):
     nginxFile = open(src, 'r+')
     nginxData = nginxFile.readlines()
+    counter = 0
     for line in nginxData:
         if "/PATH/TO/DIREC" in line:
             nginxData[counter] = line.replace("/PATH/TO/DIREC", dest)
         if "/PATH/TO/ELECTIONMANAGER" in line:
-            nginxData[counter] = line.replace("/PATH/TO/ELECTIONMANAGER", srcdirec)
+            nginxData[counter] = line.replace("/PATH/TO/ELECTIONMANAGER", rootDirProject)
+        counter = counter + 1
     nginxFile.seek(0)
     nginxFile.writelines(nginxData)
+    nginxFile.truncate()
     nginxFile.close()
 
 def writePort(src, port):
     nginxFile = open(src, 'r+')
     nginxData = nginxFile.readlines()
+    counter = 0
     for line in nginxData:
         if "DEFAULT:PORT" in line:
-            nginxData[counter] = line.replace("DEFAULT:PORT", dest)
+            nginxData[counter] = line.replace("DEFAULT:PORT", str(port))
+        counter = counter + 1
     nginxFile.seek(0)
     nginxFile.writelines(nginxData)
+    nginxFile.truncate()
     nginxFile.close()
 
 
 srcfile = os.path.realpath(__file__)
-srcdirec = os.path.split(srcfile)[0]
-nginxRootLog = srcdirec + "/nginx_config/root/log"
-nginxLog = srcdirec + "/nginx_config/handler/log"
-nginxRootFile = srcdirec + "/nginx_config/root/nginx_root.conf"
-nginxFile = srcdirec + "/nginx_config/handler/nginx_select.conf"
+rootDirProject = os.path.split(srcfile)[0]
+nginxRootLog = rootDirProject + "/nginx_config/root/log"
+nginxLog = rootDirProject + "/nginx_config/handler/log"
+nginxRootFile = rootDirProject + "/nginx_config/root/nginx_root.conf"
+nginxFile = rootDirProject + "/nginx_config/handler/nginx_select.conf"
 
 electionConfig = rootDirProject + "/_handlerConfigFiles_/handlerConfigFile.json"
 
