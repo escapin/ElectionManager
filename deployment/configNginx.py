7aabb3f7c9252fe4f8f665a527be410c92b734f9
William Standard <s4wistan@uni-trier.de>
2016-03-17 13:02:55 +0100

    updated filepaths

--- deployment/configNginx.py
+++ deployment/configNginx.py
@@ -11,8 +11,6 @@ def writeDirec(src, dest):
     for line in nginxData:
         if "/PATH/TO/DIREC" in line:
             nginxData[counter] = line.replace("/PATH/TO/DIREC", dest)
-        if "/PATH/TO/ELECTIONMANAGER" in line:
-            nginxData[counter] = line.replace("/PATH/TO/ELECTIONMANAGER", rootDirProject)
         counter = counter + 1
     nginxFile.seek(0)
     nginxFile.writelines(nginxData)
@@ -34,11 +32,10 @@ def writePort(src, port):
 
 
 srcfile = os.path.realpath(__file__)
-rootDirProject = os.path.split(srcfile)[0]
-nginxRootLog = rootDirProject + "/nginx_config/root/log"
-nginxLog = rootDirProject + "/nginx_config/handler/log"
-nginxRootFile = rootDirProject + "/nginx_config/root/nginx_root.conf"
-nginxFile = rootDirProject + "/nginx_config/handler/nginx_select.conf"
+deployDir = os.path.split(srcfile)[0]
+rootDirProject = os.path.split(deployDir)[0]
+nginxRootLog = deployDir + "/nginx_config/log"
+nginxRootFile = deployDir + "/nginx_config/nginx_root.conf"
 
 electionConfig = rootDirProject + "/_handlerConfigFiles_/handlerConfigFile.json"
 
@@ -47,12 +44,10 @@ try:
     jsonFile = open(electionConfig, 'r')
     jsonData = json.load(jsonFile, object_pairs_hook=collections.OrderedDict)
     nginxPort = jsonData["nginx-port"]
+    jsonFile.close()
 except IOError:
-    print 'Handler configuration file missing or corrupted ("nginx-port" field not found)'
+    print('Handler configuration file missing or corrupted ("nginx-port" field not found)')
 
 writeDirec(nginxRootFile, nginxRootLog)
-writeDirec(nginxFile, nginxLog)
-
 writePort(nginxRootFile, nginxPort)
-writePort(nginxFile, nginxPort)
 
