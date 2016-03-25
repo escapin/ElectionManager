d22c3af4d022f5d43753a2a61340db5d47794b93
William Standard <s4wistan@uni-trier.de>
2016-03-25 13:12:16 +0100

    changed state from "ready" to "open"

--- ElectionHandler/webapp/js/buildElectionTable.js
+++ ElectionHandler/webapp/js/buildElectionTable.js
@@ -64,7 +64,7 @@ function buildElectionTable() {
          	 stat = 'closed';
           }
           else {
-         	 stat = 'ready';
+         	 stat = 'open';
           }
           callback(eleID, stat)
         });
