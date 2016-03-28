e2632a24f24f6b78e5526ce1ae5b680c4ad20f7d
William Standard <s4wistan@uni-trier.de>
2016-03-28 17:11:00 +0200

    better output info

--- deployment/stopNginx.sh
+++ deployment/stopNginx.sh
@@ -11,3 +11,4 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 echo \* Stopping nginx services redirecting ports 80 and 433 \(requires *superuser* privileges\)...
 sudo nginx -c $DIR/nginx_config/nginx_root.conf -s quit 2>/dev/null
+echo done.
