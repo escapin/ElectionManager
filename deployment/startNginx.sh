56cebadf2bee4089e2db966813015f87e2297532
Enrico Scapin <me@escapin.net>
2016-03-30 17:11:23 +0200

    killall all nginx processes

--- deployment/startNginx.sh
+++ deployment/startNginx.sh
@@ -8,6 +8,9 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-echo \* Redirecting the traffic from ports 80 and 433 to port 8433 \(requires *superuser* privileges\)...
+echo \* Removing all nginx processes
+sudo killall nginx
+
+echo \* Redirecting the traffic from ports 80 and 433 to port 8433 \(requires *superuser* privileges\)
 sudo nginx -c $DIR/nginx_root.conf
 
