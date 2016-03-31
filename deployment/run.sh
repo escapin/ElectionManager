5dffd48d62eb9157765529578b98541a5ce3ee8f
Enrico Scapin <me@escapin.net>
2016-03-31 09:32:06 +0200

    update run.sh

--- deployment/run.sh
+++ deployment/run.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-
 echo \* Removing all nginx processes...
 sudo killall nginx
 echo done!
 
-echo \* Redirecting the traffic from ports 80 and 433 to port 8433 \(it requires *superuser* privileges\)...
-sudo nginx -c $DIR/nginx_root.conf
-echo done!
+# start nginx redirecting the traffic to the port 8433
+./startNginx.sh
 
 # Run the Election Manager
 cd ..
