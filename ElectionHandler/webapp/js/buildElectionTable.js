9aba717d523eca1225845ac54ad545495ace1cc4
William Standard <s4wistan@uni-trier.de>
2016-04-01 11:57:31 +0200

    escaping HTML and displaying browser time for elections

--- ElectionHandler/webapp/js/buildElectionTable.js
+++ ElectionHandler/webapp/js/buildElectionTable.js
@@ -2,44 +2,83 @@
 //////////Builds the HTML Table from ElectionIDs
 
 function buildElectionTable(res) {
+	
+	// expected time format is 'yy.mm.dd h:min [options not used yet]' in UTC+000 
+	var resolveTime = function(time){
+	    var dateTime = time.split(" ");
+	    var date = dateTime[0].split(".");
+	    var clientDate = new Date(date[0]+"-"+date[1]+"-"+date[2]+"T"+dateTime[1]);
+	    clientDate = new Date(clientDate.setTime(clientDate.getTime()-clientDate.getTimezoneOffset()*60000))
+	    
+		var month = clientDate.getMonth()+1<10 ? "0"+(clientDate.getMonth()+1) : (clientDate.getMonth()+1);
+		var day = clientDate.getDate()+1<10 ? "0"+clientDate.getDate() : clientDate.getDate();
+		
+		var hours = clientDate.getHours()	
+		var dt = (hours>=12)?"PM":"AM";
+		hours = (hours%12==0)?12:(hours%12);
+				
+		date = clientDate.getFullYear()+"."+month+"."+day;
+		time = hours+":"+clientDate.getMinutes()+" "+dt;
+		dateTime = date + " " + time;
+		
+	    return dateTime;
+	}
+
+    //////////////////////////////////////////////////////////////////////////////
+    /// Escape HTML
+    var MAP = { '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;',
+            '"': '&quot;',
+            "'": '&#39;'};
+
+    function escapeHTML(s, forAttribute) {
+    	return s.replace(forAttribute ? /[&<>'"]/g : /[&<>]/g, function(c) {
+    		return MAP[c];
+    	});
+    }
+	
+    //////////////////////////////////////////////////////////////////////////////
+    /// Build the election table
+	var electionConf = JSON.parse(electionConfigRaw);	
+	var elections = electionConf.elections;
+	
+	var lastMix = "http://localhost:"+electionConf["nginx-port"];
+	//don't use port 80 if it's not deployed
+	if(electionConf.deployment === true){
+		var sAddresses = JSON.parse(sAddressesRaw);
+		lastMix = sAddresses["server-address"].mix2;
+	}
 	 
-	 var electionConf = JSON.parse(electionConfigRaw);	
-	 var elections = electionConf.elections;
-	 
-	 var lastMix = "http://localhost:"+electionConf["nginx-port"];
-	 //don't use port 80 if it's not deployed
-	 if(electionConf.deployment === true){
-		 var sAddresses = JSON.parse(sAddressesRaw);
-		 lastMix = sAddresses["server-address"].mix2;
-	 }
-	 
-	 document.getElementById("elections").innerHTML = "";
-	 	 
-	 var head$ = $('<tr/>');
-	 head$.append($('<th style="text-align:center"/>').html(" Election IDs "));
-	 head$.append($('<th style="text-align:center"/>').html(" Election Title "));
-	 head$.append($('<th style="text-align:center"/>').html(" Starting Time "));
-	 head$.append($('<th style="text-align:center"/>').html(" Ending Time "));
-	 head$.append($('<th style="text-align:center"/>').html(" Election State "));
-  $("#elections").append(head$);
+	document.getElementById("elections").innerHTML = "";
+		 
+	var head$ = $('<tr/>');
+	head$.append($('<th style="text-align:center"/>').html(" Election IDs "));
+	head$.append($('<th style="text-align:center"/>').html(" Election Title "));
+	head$.append($('<th style="text-align:center"/>').html(" Starting Time "));
+	head$.append($('<th style="text-align:center"/>').html(" Ending Time "));
+	head$.append($('<th style="text-align:center"/>').html(" Election State "));
+	$("#elections").append(head$);
   
-  for (var i = 0 ; i < elections.length ; i++) {
- 	 var elecID = elections[i].electionID;
- 	 var elecStatus = 'waiting';
-  
-      var row$ = $('<tr/>');
+	for (var i = 0 ; i < elections.length ; i++) {
+		var elecID = elections[i].electionID;
+		var elecStatus = 'waiting';
+		var startingTime = resolveTime(elections[i].startTime)
+		var endingTime = resolveTime(elections[i].endTime)
+		var row$ = $('<tr/>');
       
-      row$.append($('<td style="text-align:center"/>').html("&nbsp;&nbsp;&nbsp;"+elections[i].electionID+" &nbsp;&nbsp;&nbsp;"));
-      row$.append($('<td style="text-align:center"/>').html("&nbsp;&nbsp;&nbsp;"+elections[i].electionTitle+"&nbsp;&nbsp;&nbsp;"));
-      row$.append($('<td style="text-align:center"/>').html("&nbsp;&nbsp;&nbsp;"+elections[i].startTime.substring(0, elections[i].startTime.length-9)+"&nbsp;&nbsp;&nbsp;"));
-      row$.append($('<td style="text-align:center"/>').html("&nbsp;&nbsp;&nbsp;"+elections[i].endTime.substring(0, elections[i].endTime.length-9)+"&nbsp;&nbsp;&nbsp;"));
-      row$.append($('<td style="text-align:center" id='+elecID+'/>').html("&nbsp;&nbsp;&nbsp;"+elecStatus+"&nbsp;&nbsp;&nbsp;"));
-      $("#elections").append(row$);
+		row$.append($('<td style="text-align:center"/>').html("&nbsp;&nbsp;&nbsp;"+elections[i].electionID+" &nbsp;&nbsp;&nbsp;"));
+		row$.append($('<td style="text-align:center"/>').html("&nbsp;&nbsp;&nbsp;"+escapeHTML(elections[i].electionTitle, true)+"&nbsp;&nbsp;&nbsp;"));
+		row$.append($('<td style="text-align:center"/>').html("&nbsp;&nbsp;&nbsp;"+startingTime+"&nbsp;&nbsp;&nbsp;"));
+		row$.append($('<td style="text-align:center"/>').html("&nbsp;&nbsp;&nbsp;"+endingTime+"&nbsp;&nbsp;&nbsp;"));
+		row$.append($('<td style="text-align:center" id='+elecID+'/>').html("&nbsp;&nbsp;&nbsp;"+elecStatus+"&nbsp;&nbsp;&nbsp;"));
+		$("#elections").append(row$);
       
-      getElectionStatus(elecID, function (eleID, stat){
- 		 document.getElementById(eleID).innerHTML = stat;
- 	 });
-  }     
+		getElectionStatus(elecID, function (eleID, stat){
+			document.getElementById(eleID).innerHTML = stat;
+		});
+      
+	}     
   
   
   function getElectionStatus(eleID, callback) {
@@ -71,6 +110,14 @@ function buildElectionTable(res) {
 
   }
   
+  // expected time format is 'yy.mm.dd h:min [options not used yet]' in UTC+000 
+  var resolveTime = function(time){
+      var dateTime = time.split(" ");
+      var date = dateTime[0].split(".");
+      var clientDate = new Date(date[0]+"-"+date[1]+"-"+date[2]+"T"+dateTime[1]);
+      clientDate = new Date(clientDate.setTime(clientDate.getTime()-clientDate.getTimezoneOffset()*60000))
+      return clientDate;
+  }
   
   ////////////////////////////////////////////////////////////////
   ///////// Refresh State
