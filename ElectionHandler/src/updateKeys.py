259f61b099fc75326097f0d59276c43801081904
William Standard <s4wistan@uni-trier.de>
2016-05-17 12:57:21 +0200

    removed printing out all keys

--- ElectionHandler/src/updateKeys.py
+++ ElectionHandler/src/updateKeys.py
@@ -21,23 +21,21 @@ tools_path = dstroot + "/tools"
 # to be run after the 'config_mix[0-9]+.json' files are in the current folder
 def updateMixKeys():
     files = os.listdir(dstroot+"/mix")
-    print files
     p=re.compile(pattern)
     configMix_files=filter(p.search, files);
     for file in configMix_files:
-        print "***\t" + file + "/config.json" + "\t***\n";
+        #print "***\t" + file + "/config.json" + "\t***\n";
         subprocess.call(["node", os.path.join(tools_path,keyGeneratorMix_file),
                 os.path.join(dstroot,manifest), os.path.join(dstroot,"mix/"+file+"/config.json")]);
-        print;
+        #print;
         
 def updateCSKeys():
-    print "***\t" + collectingConf + "\t***\n";
+    #print "***\t" + collectingConf + "\t***\n";
     subprocess.call(["node", os.path.join(tools_path,keyGeneratorCS_file),
         os.path.join(dstroot,manifest), os.path.join(dstroot,collectingConf)]);
-    print;
+    #print;
 
-print
+
 updateMixKeys()
-print
 updateCSKeys()
 
