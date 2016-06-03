af132d056d613c855ac31ed0d1b070fe8a799e88
William Standard <s4wistan@uni-trier.de>
2016-06-03 08:42:37 +0200

    Attempt finding open port in case of EADDRINUSE
    Spawns a child process (python script) to reconfigure
    the configuration files of nginx and the server
    before attempting to start again.

--- ElectionHandler/server.js
+++ ElectionHandler/server.js
@@ -5,6 +5,7 @@ var read = require('read');
 var fs = require('fs');
 var http = require('http');
 var mkdirp = require('mkdirp');
+var net = require('net');
 
 //var https = require('https');
 //var certificate = fs.readFileSync("../deployment/cert/select.chained.crt", 'utf8');
@@ -31,14 +32,19 @@ var numMix = 0;
 var oldSession = spawn('python', ['src/resumeElection.py']);
 oldSession.stdout.on('data', function (data) {
 	if(String(data).indexOf("OTP")>-1){
-		console.log('stdout: ' + data);
+		console.log('resume stdout: ' + data);
 	}
 	else if(String(data).indexOf("TLS")>-1){
-		console.log('mix stdout: ' + data);
+		console.log('resume mix stdout: ' + data);
 	}
 });
 oldSession.stderr.on('data', function (data) {
-    console.log('stderr: ' + data);
+    console.log('resume stderr: ' + data);
+	if(String(data).indexOf("EADDRINUSE")>-1){
+		var errorPort = String(data).split(":::");
+		errorPort = parseInt(errorPort[1].split("\n")[0]);
+		respawnServer(errorPort);
+	}
 });
 
 app.post('/election', function(req, res) {
@@ -60,7 +66,9 @@ app.post('/election', function(req, res) {
 		var rangePorts = handlerConfigFile["available-ports"];
 	    var usingPorts = handlerConfigFile["usedPorts"];
 	    var newPorts = [];
-		for(var i = rangePorts[0]; i < rangePorts[1]; i++){
+		
+	    /**
+	    for(var i = rangePorts[0]; i <= rangePorts[1]; i++){
 			if(usingPorts.indexOf(i) < 0){
 				usingPorts.push(i);
 				newPorts.push(i);
@@ -69,6 +77,17 @@ app.post('/election', function(req, res) {
 				break;
 			}
 		}
+		**/
+	    for(var i = rangePorts[0]; i <= rangePorts[1]; i++){
+	    	if(usingPorts.indexOf(i) < 0 && !portInUse(i)){
+    			usingPorts.push(i);
+				newPorts.push(i);
+	    	}
+	    	if(newPorts.length >= 3+numMix){
+				break;
+			}
+	    }
+	    
 	    if (newPorts.length < 3+numMix)
 	        res.end("Not enough ports available.");
 	    else{
@@ -77,6 +96,8 @@ app.post('/election', function(req, res) {
 	    	fs.writeFileSync("../_handlerConfigFiles_/handlerConfigFile.json", JSON.stringify(handlerConfigFile, null, 4), {spaces:4});
 	    }
 	    var ports = JSON.stringify({usedPorts: newPorts, electionsCreated: incr});
+	 
+	    
 	    
 	    //hash password
 		var salt = bcrypt.genSaltSync(10);
@@ -88,19 +109,24 @@ app.post('/election', function(req, res) {
 		session = spawn('python', ['src/createElection.py', ports, parameters]);
 		session.stdout.on('data', function (data) {
 			if(String(data).indexOf("OTP")>-1){
-				console.log('stdout: ' + data);
+				console.log('complete stdout: ' + data);
 			}
 			else if(String(data).indexOf("TLS")>-1){
-				console.log('mix stdout: ' + data);
+				console.log('complete mix stdout: ' + data);
 			}
 		});
 		session.stderr.on('data', function (data) {
-		    console.log('stderr: ' + data);
+		    console.log('complete stderr: ' + data);
+			if(String(data).indexOf("EADDRINUSE")>-1){
+				var errorPort = String(data).split(":::");
+				errorPort = parseInt(errorPort[1].split("\n")[0]);
+				respawnServer(errorPort);
+			}
 		    res.end(data);
 		});
 
 		session.on('exit', function (code) {
-		    console.log('child process exited with code ' + code);
+		    console.log('complete child process exited with code ' + code);
 		    if(code === 0){
 		    	res.end("created");
 		    }
@@ -121,15 +147,15 @@ app.post('/election', function(req, res) {
 		var rangePorts = handlerConfigFile["available-ports"];
 	    var usingPorts = handlerConfigFile["usedPorts"];
 	    var newPorts = [];
-		for(var i = rangePorts[0]; i < rangePorts[1]; i++){
-			if(usingPorts.indexOf(i) < 0){
-				usingPorts.push(i);
+	    for(var i = rangePorts[0]; i <= rangePorts[1]; i++){
+	    	if(usingPorts.indexOf(i) < 0 && !portInUse(i)){
+    			usingPorts.push(i);
 				newPorts.push(i);
-			}
-			if(newPorts.length >= 3+numMix){
+	    	}
+	    	if(newPorts.length >= 3+numMix){
 				break;
 			}
-		}
+	    }
 	    if (newPorts.length < 3+numMix)
 	        res.end("Not enough ports available.");
 	    else{
@@ -143,19 +169,24 @@ app.post('/election', function(req, res) {
 	    session = spawn('python', ['src/createElection.py', ports]);
 		session.stdout.on('data', function (data) {
 			if(String(data).indexOf("OTP")>-1){
-				console.log('stdout: ' + data);
+				console.log('simple stdout: ' + data);
 			}
 			else if(String(data).indexOf("TLS")>-1){
-				console.log('mix stdout: ' + data);
+				console.log('simple mix stdout: ' + data);
 			}
 		});
 		session.stderr.on('data', function (data) {
-		    console.log('stderr: ' + data);
+		    console.log('simple stderr: ' + data);
+			if(String(data).indexOf("EADDRINUSE")>-1){
+				var errorPort = String(data).split(":::");
+				errorPort = parseInt(errorPort[1].split("\n")[0]);
+				respawnServer(errorPort);
+			}
 		    res.end(data);
 		});
 
 		session.on('exit', function (code) {
-		    console.log('child process exited with code ' + code);
+		    console.log('simple child process exited with code ' + code);
 		    if(code === 0){
 		    	res.end("created");
 		    }
@@ -182,15 +213,15 @@ app.post('/election', function(req, res) {
 		//call the python script to shutdown the servers
 		session = spawn('python', ['src/removeElection.py', value, pass]);
 		session.stdout.on('data', function (data) {
-			console.log('stdout: ' + data);
+			console.log('remove stdout: ' + data);
 		});
 		session.stderr.on('data', function (data) {
-		    console.log('stderr: ' + data);
+		    console.log('remove stderr: ' + data);
 		    res.end(data);
 		});
 
 		session.on('exit', function (code) {
-		    console.log('child process exited with code ' + code);
+		    console.log('remove child process exited with code ' + code);
 		    if(code === 0){
 		    	res.end("removed");
 		    }
@@ -201,6 +232,69 @@ app.post('/election', function(req, res) {
 	}
 });
 
+var portInUse = function(port){
+	var ghost = net.createServer();
+	ghost.listen(port, function(err){
+		ghost.once('close', function(){
+			return false;
+		});
+		ghost.close();
+	});
+	ghost.on('error', function(err){
+		if(err.code !== 'EADDRINUSE'){
+			return err;
+		}
+		return true;
+	})
+	ghost.close()
+	return false;
+};
+
+
+function respawnServer(errPort){
+	console.log("\nPort " + errPort + " in use, attempting to start server on different port:")
+
+	// get available ports and mark them as used, sync 
+	var handlerConfigFile = JSON.parse(fs.readFileSync("../_handlerConfigFiles_/handlerConfigFile.json"));
+	var rangePorts = handlerConfigFile["available-ports"];
+    var usingPorts = handlerConfigFile["usedPorts"];
+    var newPort = -1;
+    for(var i = rangePorts[0]; i <= rangePorts[1]; i++){
+    	if(usingPorts.indexOf(i) < 0 && !portInUse(i)){
+    		newPort = i;
+    		break;
+    	}
+    }
+	if(newPort < 0){
+		console.log("Not enough ports available.");
+		return;
+	}
+	//remove blocked Port from list of ports being used
+	usingPorts.push(newPort);
+	handlerConfigFile["usedPorts"] = usingPorts;
+	fs.writeFileSync("../_handlerConfigFiles_/handlerConfigFile.json", JSON.stringify(handlerConfigFile, null, 4), {spaces:4});
+	console.log("Trying port " + newPort)
+
+	//start new server with different port
+    var reSession = spawn('python', ['src/restartServer.py', errPort, newPort]);
+    reSession.stdout.on('data', function (data) {
+    	console.log('reSpawn stdout: ' + data);
+		if(String(data).indexOf("OTP")>-1){
+			console.log('reSpawn stdout: ' + data);
+		}
+		else if(String(data).indexOf("TLS")>-1){
+			console.log('mix stdout: ' + data);
+		}
+	});
+    reSession.stderr.on('data', function (data) {
+		if(String(data).indexOf("EADDRINUSE")>-1){
+			var errorPort = String(data).split(":::");
+			errorPort = parseInt(errorPort[1].split("\n")[0]);
+			respawnServer(errorPort);
+		}
+	    console.log('reSpawn stderr: ' + data);
+	});
+}
 
 // Test if the file with stored passwords exists and is a valid json file
 try{
