a270e16abcad67f222ed7f3fbd373ffe509eedad
William Standard <s4wistan@uni-trier.de>
2016-06-14 14:25:30 +0200

    preperations to include ssl option

--- ElectionHandler/src/createElection.py
+++ ElectionHandler/src/createElection.py
@@ -440,14 +440,14 @@ if "http://localhost" not in serverAddress["collectingserver"]:
     comments = ["  # Voting Booth " + electionID + " \n", "  server {\n", 
                 "    listen " + str(ELS) + " ssl;\n", "\n", 
                 "    access_log " + nginxLog +"/access.log;\n", 
-                "    error_log " + nginxLog +"/error.log;\n", "\n", 
-                "    server_name "+ serverAddress["votingbooth"].split("://")[1].split("/")[0] + ";\n", "\n",
-                "    ssl_certificate /home/select/ElectionManager/deployment/cert/subdomains.select.chained.crt;\n",
+                "    error_log " + nginxLog +"/error.log;\n", "\n",
+                "    server_name "+ serverAddress["votingbooth"].split("://")[1].split("/")[0] + ";\n", "\n"]
+    comments.extend(["    ssl_certificate /home/select/ElectionManager/deployment/cert/subdomains.select.chained.crt;\n",
                 "    ssl_certificate_key /home/select/ElectionManager/deployment/cert/subdomains.select.unencrypted.key;\n",
                 "    ssl_protocols       TLSv1 TLSv1.1 TLSv1.2;\n",
                 "    ssl_ciphers         HIGH:!aNULL:!MD5;\n", "\n",
-                "    proxy_set_header X-Forwarded-For $remote_addr;\n", "\n",
-                "    location " + "/ {\n", "        alias " + dstroot + "/VotingBooth/webapp/;\n", "        index votingBooth.html;\n","    }\n", "\n", "  }\n", "\n"]
+                "    proxy_set_header X-Forwarded-For $remote_addr;\n", "\n"])
+    comments.extend(["    location " + "/ {\n", "        alias " + dstroot + "/VotingBooth/webapp/;\n", "        index votingBooth.html;\n","    }\n", "\n", "  }\n", "\n"])
     comments.extend(bracketIt)
     nginxData.extend(comments)
     nginxFile.seek(0)
