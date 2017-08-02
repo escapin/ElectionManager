cd2ddfe27dd7a950f6eb6d5df394482056d22cab
Enrico Scapin <me@escapin.net>
2017-08-02 16:32:08 +0200

    startNginx.sh as a separated file from run.sh

---
+++ startNginx.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# getting the file location of the file itself
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" 
+# if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+
+
+echo Start the nginx services managing the servers of sElect
+/usr/sbin/nginx -c $DIR/nginx_config/nginx_select.conf 2>/dev/null
+/usr/sbin/nginx -c $DIR/nginx_config/nginx_select.conf -s reload 2>/dev/null
