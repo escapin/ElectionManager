cbbc891d8269b3cd39a7de5d488f719e652af683
Enrico Scapin <me@escapin.net>
2017-08-04 14:46:56 +0200

    [Election Handler] bug in the scripts corrected

--- ElectionHandler/run.sh
+++ ElectionHandler/run.sh
@@ -1,6 +1,6 @@
-node ../sElect/tools/config2js.js config.json > webapp/js/config.js configRaw
-node ../sElect/tools/config2js.js ../_configFiles_/handlerConfigFile.json > webapp/js/ElectionConfigFile.js electionConfigRaw
-node ../sElect/tools/config2js.js ../_configFiles_/serverAddresses.json > webapp/js/serverAddresses.js sAddressesRaw
+#!/bin/bash
+
+./refreshConfig.sh
 
 echo Starting the Election Handler...
 node server.js
