f4af69bae390e0864d9c4dac83b2f566a9b7a403
Enrico Scapin <me@escapin.net>
2017-08-04 15:37:02 +0200

    correcting again some scripts

--- ElectionHandler/refreshConfig.sh
+++ ElectionHandler/refreshConfig.sh
@@ -4,6 +4,6 @@ echo Refreshing the Election Handler\'s config files...
 
 node ../sElect/tools/config2js.js config.json > webapp/js/config.js configRaw
 node ../sElect/tools/config2js.js ../_configFiles_/handlerConfigFile.json > webapp/js/ElectionConfigFile.js electionConfigRaw
-node ../sElect/tools/config2js.js ../_configFiles_/serverAddresses.json > webapp/js/serverAddresses.js sAddressesRaw
+node ../sElect/tools/config2js.js ../_configFiles_/serverAddresses.json > webapp/js/serverAddresses.js sAddressesRaw 2>/dev/null
 
 
